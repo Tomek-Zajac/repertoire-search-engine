1504bf2a4695e5e74c168266fe354353e508f472
Tomasz Zajac <tomasz.zajac@pb.com>
2023-04-14 07:50:03 +0200

    Changed dockerfile

--- src/UserManagementService/UserManagementService.Api/Program.cs
+++ src/UserManagementService/UserManagementService.Api/Program.cs
@@ -6,8 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
